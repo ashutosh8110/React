68e7bb470dcee79d27059a6b2fdf287b8354e82d
Ashutosh Sharma <ashutosh8110@gmail.com>
2021-08-29 19:10:21 +0530

    Splitting Components into multiple files

--- 01-starting-setup/src/App.js
+++ 01-starting-setup/src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import ExpenseItem from './Components/ExpenseItem';
+import NewExpenseItem from './Components/NewExpenseItem';
 import './App.css';
 
 function App() {
@@ -27,27 +27,8 @@ function App() {
           </a>
         </header>
       </div>
-      <ExpenseItem 
-        title={expenses[0].title} 
-        amount={expenses[0].amount} 
-        date={expenses[0].date}>
-      </ExpenseItem>
-      <ExpenseItem 
-        title={expenses[1].title} 
-        amount={expenses[1].amount} 
-        date={expenses[1].date}>
-      </ExpenseItem>
-      <ExpenseItem 
-        title={expenses[2].title} 
-        amount={expenses[2].amount} 
-        date={expenses[2].date}>
-      </ExpenseItem>
-      <ExpenseItem 
-        title={expenses[3].title} 
-        amount={expenses[3].amount} 
-        date={expenses[3].date}>
-      </ExpenseItem>
-    </div>
+      <NewExpenseItem expenses={expenses}/>
+      </div>
   );
 }
 
