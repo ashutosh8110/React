828c2f5bf3016870f394b1709a8976e1e3a0ab6c
Ashutosh Sharma <ashutosh8110@gmail.com>
2021-09-05 12:42:07 +0530

    changing states on click

--- 01-starting-setup/src/Components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/Components/Expenses/ExpenseItem.js
@@ -1,25 +1,23 @@
+import react,{ useState } from 'react';
 import Card from '../UI/Card.js'
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate.js'
 
 
 function ExpenseItem(props){
-    function* clickHandler(){
-        var i=0;
-        while(true){
-          yield i;
-          i=i+1;
-        }
+    const [title,seTitle]=useState(props.title);
+    function clickHandler(){
+        seTitle("updated");
+        console.log(title);
       } 
-      let generator= clickHandler();
     return (
         <Card className="expense-item" >
             <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className="expense-item__price">{'$'+props.amount}</div>
             </div>
-            <button onClick={()=>{console.log('Clicked ' + generator.next().value)}}>Change Title</button>
+            <button onClick={clickHandler}>Change Title</button>
     
         </Card>
     );
