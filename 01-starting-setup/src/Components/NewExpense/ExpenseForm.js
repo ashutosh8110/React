821037c1de2cab2f4e4eab03cee876c63caa5d88
Ashutosh Sharma <ashutosh8110@gmail.com>
2021-09-12 16:49:01 +0530

    Using a single object for multiple states

--- 01-starting-setup/src/Components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/Components/NewExpense/ExpenseForm.js
@@ -2,25 +2,36 @@ import React,{ useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-    const [enteredTitle,setEnteredTitle]=useState('');
-    const [enteredAmt,setEnteredAmt]=useState(0);
-    const [enteredDate,setEnteredDate]=useState();
+    const [userInput,setUserInput]=useState({
+        enteredTitle:'',
+        enteredAmt:'',
+        enteredDate:''
+    });
     
     const titleChangeHandler = (event) => {
         console.log('Title Changed!');
-        setEnteredTitle(event.target.value);
-        console.log(enteredTitle);
+        setUserInput({
+            ...userInput,
+            enteredTitle:event.target.value
+        });
+        console.log(userInput);
     };
     const amtChangeHandler = (event) => {
         console.log('Amount Changed!');
-        setEnteredAmt(event.target.value);
-        console.log(enteredAmt);
+        setUserInput({
+            ...userInput,
+            enteredAmt:event.target.value
+        });
+        console.log(userInput);
     };
 
     const dateChangeHandler = (event) => {
-        console.log('Amount Changed!');
-        setEnteredDate(event.target.value);
-        console.log(enteredDate);
+        console.log('Date Changed!');
+        setUserInput({
+            ...userInput,
+            enteredDate:event.target.value
+        });
+        console.log(userInput);
     };
     
     return (
