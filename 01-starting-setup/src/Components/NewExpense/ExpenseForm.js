698e594ebc66bf18454ed52fa9594ccb534fb613
Ashutosh Sharma <ashutosh8110@gmail.com>
2021-09-12 16:38:46 +0530

    Adding multiple states

--- 01-starting-setup/src/Components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/Components/NewExpense/ExpenseForm.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+    const [enteredTitle,setEnteredTitle]=useState('');
+    const [enteredAmt,setEnteredAmt]=useState(0);
+    const [enteredDate,setEnteredDate]=useState();
+    
     const titleChangeHandler = (event) => {
         console.log('Title Changed!');
-        console.log(event.target.value);
+        setEnteredTitle(event.target.value);
+        console.log(enteredTitle);
     };
     const amtChangeHandler = (event) => {
         console.log('Amount Changed!');
-        console.log(event.target.value);
+        setEnteredAmt(event.target.value);
+        console.log(enteredAmt);
+    };
+
+    const dateChangeHandler = (event) => {
+        console.log('Amount Changed!');
+        setEnteredDate(event.target.value);
+        console.log(enteredDate);
     };
     
     return (
@@ -26,7 +38,7 @@ const ExpenseForm = () => {
 
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min="2019-01-01" max="2022-12-31"/>
+                    <input type='date' min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
                 </div>
                 
                 <div className="new-expense__actions">
