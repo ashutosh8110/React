c92ee10017bbd81d796aab0fe8101a3652eb202f
Ashutosh Sharma <ashutosh8110@gmail.com>
2021-09-12 13:44:29 +0530

    Listening to the user event

--- 01-starting-setup/src/Components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/Components/NewExpense/ExpenseForm.js
@@ -2,17 +2,26 @@ import React from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+    const titleChangeHandler = (event) => {
+        console.log('Title Changed!');
+        console.log(event.target.value);
+    };
+    const amtChangeHandler = (event) => {
+        console.log('Amount Changed!');
+        console.log(event.target.value);
+    };
+    
     return (
         <form>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text'/>
+                    <input type='text' onChange={titleChangeHandler}/>
                 </div>
 
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' min="0.01" step="0.01"/>
+                    <input type='number' min="0.01" step="0.01" onChange={amtChangeHandler}/>
                 </div>
 
                 <div className='new-expense__control'>
