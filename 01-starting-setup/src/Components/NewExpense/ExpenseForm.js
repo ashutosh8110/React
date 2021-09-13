5bcbb3aaa62ee58a7609c1a0bd729ba2bbf80a81
Ashutosh Sharma <ashutosh8110@gmail.com>
2021-09-13 10:48:09 +0530

    Adding two way Binding

--- 01-starting-setup/src/Components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/Components/NewExpense/ExpenseForm.js
@@ -32,22 +32,50 @@ const ExpenseForm = () => {
         console.log(userInput);
     };
     
+    const submitHandler = (event) => {
+        event.preventDefault();
+        console.log(userInput);
+        setUserInput(()=>{
+            return {
+                enteredTitle:'',
+                enteredAmt:'',
+                enteredDate:''
+            };
+        });
+    }
+
     return (
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' onChange={titleChangeHandler}/>
+                    <input 
+                        type='text' 
+                        value={userInput.enteredTitle} 
+                        onChange={titleChangeHandler}
+                    />
                 </div>
 
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' min="0.01" step="0.01" onChange={amtChangeHandler}/>
+                    <input 
+                        type='number' 
+                        min="0.01" 
+                        step="0.01" 
+                        value={userInput.enteredAmt} 
+                        onChange={amtChangeHandler}
+                    />
                 </div>
 
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+                    <input 
+                        type='date' 
+                        min="2019-01-01" 
+                        max="2022-12-31" 
+                        value={userInput.enteredDate} 
+                        onChange={dateChangeHandler}
+                    />
                 </div>
                 
                 <div className="new-expense__actions">
