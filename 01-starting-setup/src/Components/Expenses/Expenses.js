3ad7194ff8f0d37ffca086c892fce5badefb67d2
Ashutosh Sharma <ashutosh8110@gmail.com>
2021-10-05 09:28:10 +0530

    Adding conditionals to render text when items list is empty

--- 01-starting-setup/src/Components/Expenses/Expenses.js
+++ 01-starting-setup/src/Components/Expenses/Expenses.js
@@ -6,27 +6,38 @@ import './Expenses.css'
 
 const Expenses = (props) => {
     const [ filteredyear, setFilteredYear ] = useState('2020');
-
     const filterChangeHandler = selectedYear =>{
         setFilteredYear(selectedYear);
-        
     }
-    const expenses=props.expenses;
+
+    const filteredExpenses = props.expenses.filter(x => {
+        return x.date.getFullYear().toString() === filteredyear
+    });
+
+    let expenseContent = <p>No Expense found</p>
+
+    if(filteredExpenses.length >0){
+        expenseContent= filteredExpenses.map(expense => (
+            <ExpenseItem 
+                key={expense.id}
+                title={expense.title} 
+                amount={expense.amount} 
+                date={expense.date}
+            />
+        ))
+    }
+
     return (
         <div>
             <Card className="expenses">
-                <ExpensesFilter selected={filteredyear} onChangeFilter = {filterChangeHandler}/>
-                {expenses.map(expense => (
-                    <ExpenseItem 
-                        key={expense.id}
-                        title={expense.title} 
-                        amount={expense.amount} 
-                        date={expense.date}
-                    />
-                ))}
+                <ExpensesFilter 
+                    selected={filteredyear} 
+                    onChangeFilter = {filterChangeHandler}
+                />
+                {expenseContent}
             </Card>
         </div>
-        )
+    );
 };
 
 export default Expenses;
